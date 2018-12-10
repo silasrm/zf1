85f55dffe9f36f876fe5bc8d67e503a2e3fbeba2
Silas Ribas Martins <silasrm@gmail.com>
2018-12-10 14:36:42 -0300

    Fix!

--- library/Zend/View/Abstract.php
+++ library/Zend/View/Abstract.php
@@ -903,7 +903,7 @@ abstract class Zend_View_Abstract implements Zend_View_Interface
     {
         if (in_array($this->_escape, array('htmlspecialchars', 'htmlentities'))) {
             if (is_array($var)) {
-                return implode(null, $var);
+                return call_user_func($this->_escape, array_shift($var), ENT_COMPAT, $this->_encoding);
             }
             
             return call_user_func($this->_escape, $var, ENT_COMPAT, $this->_encoding);
